9aa26027dedfa85397038c6f884926d4751c1742
yeeed711 <rladpwl910@naver.com>
2022-03-12 16:03:12 +0900

    Minutes To Hours Converter
    시간/분 단위 변환기

--- react/react-movie-app/src/App.js
+++ react/react-movie-app/src/App.js
@@ -1,17 +1,16 @@
 import { useState, useEffect } from "react";
 
-function App() {
+function MinutesToHours() {
   const [amount, setAmount] = useState(0);
   const [inverted, setInverted] = useState(false);
   const onChange = (event) => setAmount(event.target.value);
   const reset = () => setAmount("");
-  const onFlip = () => {
+  const onInvert = () => {
     setInverted((current) => !current);
     reset();
   };
   return (
     <div>
-      <h1>Super Converter</h1>
       <div>
         <label htmlFor="minutes">Minutes : </label>
         <input
@@ -35,7 +34,29 @@ function App() {
         />
       </div>
       <button onClick={reset}>Reset</button>
-      <button onClick={onFlip}>Flip</button>
+      <button onClick={onInvert}>Flip</button>
+    </div>
+  );
+}
+function KmToMiles() {
+  return <h3>KM 2 M</h3>;
+}
+
+function App() {
+  const [index, setIndex] = useState("xx");
+  const onSelect = (event) => setIndex(event.target.value);
+  return (
+    <div>
+      <h1>Super Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value={"xx"}>변환기를 선택해주세요</option>
+        <option value={"0"}>Minutes & Hours</option>
+        <option value={"1"}>Km & Miles</option>
+      </select>
+      <hr />
+      {index === "xx" ? "변환기를 선택해주세요 !" : null}
+      {index === "0" ? <MinutesToHours /> : null}
+      {index === "1" ? <KmToMiles /> : null}
     </div>
   );
 }
