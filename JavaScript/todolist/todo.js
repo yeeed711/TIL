85d1cdfeeb69dcaf1ee40acdcf0ece1c155de9f6
yeeed711 <rladpwl910@naver.com>
2022-03-14 14:55:14 +0900

    todo delete
    remove 기능 추가

--- JavaScript/todolist/todo.js
+++ JavaScript/todolist/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement; // 콘솔로 이벤트 발생하는 부분이 어딘지 찾기
+  li.remove(); //지워주기만 하면 끝
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li"); //투두 값을 추가할 때마다 리스트로 표현 할 것이기 때문에 리스트 태그를 생성
   const span = document.createElement("span"); //span태그안에 투두 값을 저장하기 위해 생성, 추후 삭제버튼 또한 추가해야한다. 이 모든건 li태그안에 있어야하므로 따로따로 생성할 것.
-  li.appendChild(span); //appendChild는 객체를 추가해주는 기능 한 번에 오직 하나의 노드만 추가가능하다.
   span.innerText = newTodo; //span에 newTodo를 추가한다. newTodo는 인풋에 저장되는 값.
+  const button = document.createElement("button"); //삭제 버튼을 생성한다
+  button.innerText = "✖️";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span); //appendChild는 객체를 추가해주는 기능 한 번에 오직 하나의 노드만 추가가능하다.
+  li.appendChild(button); //순서대로 추가해주기 위함.
   toDoList.appendChild(li); //리스트에 만들어준 li를 추가한다.
 }
 
