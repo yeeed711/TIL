c938d124ebb34206585bee7a3d3ad0511533b4b9
yeeed711 <rladpwl910@naver.com>
2022-03-14 16:37:15 +0900

    Loading to dos
    로컬스토리지에 저장하는 기능 추가

--- JavaScript/todolist/todo.js
+++ JavaScript/todolist/todo.js
@@ -1,6 +1,15 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+const TODOS_KYE = "todos"; // 특정 단어를 두번 이상 사용한다면 변수로 저장
+
+let toDos = []; // 입력한 투투값을 저장하기위해 빈 배열 생성
+//새로고침시 지난 값들도 저장하고싶기 때문에 const->let 로 변경
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KYE, JSON.stringify(toDos)); // 로컬스토리지를 이용해 웹페이지가 새로고침되더라도 값을 저장할 수 있도록 한다.
+} //JSON.stringify 매서드를 이용하면 배열형태로 된 문자열로 저장할 수 있다.
+// 로컬스토리지는 string format만 지원가능하다.
 
 function deleteToDo(event) {
   const li = event.target.parentElement; // 콘솔로 이벤트 발생하는 부분이 어딘지 찾기
@@ -23,7 +32,17 @@ function handleToDoSubmit(event) {
   event.preventDefault(); //서브밋이벤트를 설정함으로 자동새로고침 중단
   const newTodo = toDoInput.value; //투두값을 저장
   toDoInput.value = ""; //저장 후에 비움
+  toDos.push(newTodo); // paintToDo 를 그리기전에 만들어둔 빈 배열에 뉴투두를 추가한다.
   paintToDo(newTodo); //투두값을 표현해줄 함수를 실행 시키면서 그 함수의 인자로 저장한 투두값을 전달
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KYE); //로컬스토리지에서 가져온 스트링으로 변환한 문자
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
